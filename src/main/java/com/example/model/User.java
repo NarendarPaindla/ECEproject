aae82911d389cef68cf96424fa36dd6d7c171f55
narendarpaindla <narendarreddypaindla@gmail.com>
2025-09-03 05:26:41 +0000

    stage-1

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -6,4 +6,23 @@ public class User {
     private String email;
     private String password;
     private Role role;
+    private Long  managerId;
+    public User() { }
+    public User(Long id,String fullName,String email,String password,Role role,Long managerId){
+        this.id=id;
+        this.fullName=fullName;
+        this.email=email;
+        this.password=password;
+        this.role=role;
+        this.managerId=managerId;
+
+    }
+
+    //getters
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
 }
